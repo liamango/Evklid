cfeb7ef6a6dfd1f1bfed0c00db98ff0430169ef6
AdamabuMedina <adamabujamila@gmail.com>
2020-10-04 22:54:52 +0300

    Добавление  outline

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-let mySwiper = new Swiper('.swiper-container', {
+let mySwiper = new Swiper(".swiper-container", {
   slidesPerwiew: 1,
   spaceBetween: 0,
 
@@ -6,43 +6,41 @@ let mySwiper = new Swiper('.swiper-container', {
     // when window width is >= 320px
     420: {
       slidesPerView: 1,
-      spaceBetween: 5
+      spaceBetween: 5,
     },
     // when window width is >= 480px
     768: {
       slidesPerView: 1,
-      spaceBetween: 5
+      spaceBetween: 5,
     },
     // when window width is >= 640px
     1024: {
       slidesPerView: 1,
-      spaceBetween: 5
-    }
+      spaceBetween: 5,
+    },
   },
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
+    el: ".swiper-pagination",
+    type: "bullets",
     clickable: true,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-
-
-})
+});
 
 $(function () {
   $("#accordion").accordion();
 });
 
 $(document).ready(function () {
-  $('.burger').click(function (event) {
-    $('.burger,.header__nav').toggleClass('active');
-    $('body').toggleClass('lock');
+  $(".burger").click(function (event) {
+    $(".burger,.header__nav").toggleClass("active");
+    $("body").toggleClass("lock");
 
     $(this).find(".line").toggleClass("active");
     $(this).find(".line").toggleClass("hover");
