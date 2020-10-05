2e0e71170fabc9b122efb3695a87bdda52cd3e8a
AdamabuMedina <adamabujamila@gmail.com>
2020-10-05 12:33:19 +0300

    делаю плавный скролинг к якорю

--- script/main.js
+++ script/main.js
@@ -45,4 +45,12 @@ $(document).ready(function () {
     $(this).find(".line").toggleClass("active");
     $(this).find(".line").toggleClass("hover");
   });
+  $("a[href^='#']").click(function () {
+    var _href = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(_href).offset().top + "px"
+    });
+    return false;
+  });
+
 });
